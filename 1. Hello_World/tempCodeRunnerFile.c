fd3a85d0abcbbe69f83d7f227f81ac2b52239296
✪ Mr Rogue Knight | Prashant <89287357+MrRogueKnight@users.noreply.github.com>
2024-05-26 15:28:11 +0530

    Rogue

---
+++ 1. Hello_World/tempCodeRunnerFile.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+	char H = 'H';
+	char e = 'e';
+	char l1 = 'l';
+	char l2 = 'l';
+	char o = 'o';
+	char space = ' ';
+	char W = 'W';
+	char o2 = 'o';
+	char r = 'r';
+	char d = 'd';
+
+	printf("%c%c%c%c%c%c%c%c%c%c%c\n", H, e, l1, l2, o, space, W, o2, r, l1, d);
+
+	return 0;
+}
