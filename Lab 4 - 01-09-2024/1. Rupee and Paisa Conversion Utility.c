f62c0ccd81dd42f067a8999c7e34aa125a3d63e8
MrRogueKnight <89287357+MrRogueKnight@users.noreply.github.com>
2024-08-30 15:32:57 +0530

    Rogue

---
+++ Lab 4 - 01-09-2024/1. Rupee and Paisa Conversion Utility.c
@@ -0,0 +1,23 @@
+/*
+Title  : Rupees and Paisa Conversion Utility - Enter Paisa Only
+WAP to convert given Paisa into its equivalent Rupees and Paisa as per the following format.
+
+Input  : Enter the Amount:550 Paisa
+Output : 550 Paisa = 5 Rupees and 50 Paisa
+
+Steps:
+1. Taking input (Amount) form the user in Paisa.
+2. Let's say : 550 Paisa  -> Break - 2 Parts : (550 Paisa = 5 Rupees and 50 Paisa)
+3. Maths: 550 => 550/100 = 5.5 but int => 5  now we 550%100 = 55 which is Paisa.
+*/
+#include <stdio.h>
+int main()
+{
+int Rupees, Paisa, Amount;                                           // Declaring the required variables
+printf("Enter the Amount: ");                                        // Display the text
+scanf("%d",&Amount);                                                 // Input
+Rupees = Amount / 100; Paisa = Amount % 100;                         // Calculations
+printf("%d Paisa = %d Rupees and %d Paisa",Amount, Rupees, Paisa);   // Output
+getch();
+return 0;
+}
