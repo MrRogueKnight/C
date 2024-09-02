0177e517c3740a973067901fd48a22c917b7a1be
MrRogueKnight <89287357+MrRogueKnight@users.noreply.github.com>
2024-09-02 14:41:46 +0530

    Rogue

--- Lab 4 - 02-09-2024/6. Finding Quotient and Remainder.c
+++ Lab 4 - 02-09-2024/6. Finding Quotient and Remainder.c
@@ -1,7 +1,36 @@
-/*
+/**
 Title: Finding Quotient and Remainder
-WAP to perform to take two integers as input from the user and perform division of first integer by second integer using subtraction and return quotient and remainder. 
+WAP to perform to take two integers as input from the user and perform division of first integer by second
+integer using subtraction and return quotient and remainder.
 Note:first integer >= second integer. Don’t use *, /, %.
 Input : Enter two numbers: 27 5
 Output: Quotient: 5 Remainder: 2
-*/
+**/
+#include<stdio.h>
+void divide(int dividend, int divisor) {
+    int quotient = 0;
+    while (dividend >= divisor) {
+        dividend -= divisor;
+        quotient++;
+    }
+    printf("Quotient: %d\n", quotient);
+    printf("Remainder: %d\n", dividend);
+}
+
+int main(){
+    int num1, num2;
+    printf("Enter two numbers: ");
+
+    scanf("%d",&num1);
+    scanf("%d",&num2);
+
+    if(num1>=num2){
+            divide(num1, num2);
+    }
+    else{
+         printf("Note:first integer >= second integer.");
+    }
+
+getch();
+return 0;
+}
