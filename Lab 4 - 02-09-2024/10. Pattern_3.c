6a2e272bad3158f969f9606152b2122059d399db
MrRogueKnight <89287357+MrRogueKnight@users.noreply.github.com>
2024-09-02 16:35:19 +0530

    Rogue

--- Lab 4 - 02-09-2024/10. Pattern_3.c
+++ Lab 4 - 02-09-2024/10. Pattern_3.c
@@ -10,3 +10,25 @@ Input: Enter the value of n:5
 *********
 --------------
 */
+/*
+These types of patterns are called "triangle" or "pyramid" patterns. Here's a simple algorithm to print them:
+1. Determine the number of rows (height) of the pattern.
+2. Use a loop to iterate through each row.
+3. Inside the loop, use another loop to print the required number of asterisks (*) for each row.
+4. The number of asterisks will increase or decrease depending on the pattern.
+*/
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+        printf("*");
+    }
+    printf("\n");
+}
+return 0;
+}
+
--- Lab 4 - 02-09-2024/8. Pattern_1.c
+++ Lab 4 - 02-09-2024/8. Pattern_1.c
@@ -11,3 +11,17 @@ Output:
 *
 --------------
 */
+#include<stdio.h>
+int main() {
+    int n;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+    int i, j;
+    for(i=n; i>=1; i--) {
+        for(j=1; j<=i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
