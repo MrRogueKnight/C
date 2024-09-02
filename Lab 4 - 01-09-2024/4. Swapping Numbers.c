f4e8bdb2d381ee05dea31966c02095f9f34513a2
MrRogueKnight <89287357+MrRogueKnight@users.noreply.github.com>
2024-09-02 09:27:01 +0530

    Rogue

---
+++ Lab 4 - 01-09-2024/4. Swapping Numbers.c
@@ -0,0 +1,5 @@
+/*
+Write a C program to perform swapping of two integers without using a third variable.
+Input: Enter num1: 10 Enter num2: 20
+Output: Before Swapping num1=10, num2=20 After Swapping num1=20, num2=10
+*/
