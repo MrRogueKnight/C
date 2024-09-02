e8aa64a42f314ab3e1e661a3ffa573c8c3f00865
MrRogueKnight <89287357+MrRogueKnight@users.noreply.github.com>
2024-09-02 14:16:06 +0530

    Rogue

---
+++ Lab 4 - 02-09-2024/6. Finding Quotient and Remainder.c
@@ -0,0 +1,7 @@
+/*
+Title: Finding Quotient and Remainder
+WAP to perform to take two integers as input from the user and perform division of first integer by second integer using subtraction and return quotient and remainder. 
+Note:first integer >= second integer. Don’t use *, /, %.
+Input : Enter two numbers: 27 5
+Output: Quotient: 5 Remainder: 2
+*/
