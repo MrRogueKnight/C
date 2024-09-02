1b49442f641f58eeabdce879cfe29d3ddf439f42
MrRogueKnight <89287357+MrRogueKnight@users.noreply.github.com>
2024-09-02 22:38:10 +0530

    Rogue

--- Lab 4 - 02-09-2024/9. Pattern_2.c
+++ Lab 4 - 02-09-2024/9. Pattern_2.c
@@ -10,17 +10,34 @@ Input: Enter the value of n:5
 *********
 --------------
 */
-#include<stdio.h>
+#include <stdio.h>
 
-int main() {
-    int i, j, n;
-    printf("Enter the value of n: ");
-    scanf("%d", &n);
-    for(i=1; i<=n; i++) {
-        for(j=1; j<=i; j++) {
+int main()
+{
+    int i, j, rows;
+
+    /* Input number of rows to print */
+    printf("Enter number of rows : ");
+    scanf("%d", &rows);
+
+    /* Iterate through rows */
+    for(i=1; i<=rows; i++)
+    {
+        /* Print leading spaces */
+        for(j=i; j<rows; j++)
+        {
+            printf(" ");
+        }
+
+        /* Print star */
+        for(j=1; j<=(2*i-1); j++)
+        {
             printf("*");
         }
+
+        /* Move to next line */
         printf("\n");
     }
+
     return 0;
 }
