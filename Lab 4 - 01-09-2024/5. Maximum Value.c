58d14ca13776abc5ec87d834402f86deea15b61a
MrRogueKnight <89287357+MrRogueKnight@users.noreply.github.com>
2024-09-02 12:55:00 +0530

    Rogue

--- Lab 4 - 01-09-2024/5. Maximum Value.c
+++ Lab 4 - 01-09-2024/5. Maximum Value.c
@@ -1,6 +1,34 @@
-/* 
-Title : Maximum Value 
-WAP to find the largest between three numbers. 
+/**
+Title : Maximum Value
+WAP to find the largest between three numbers.
 Input: Enter three numbers: 80 105 990
 Output: The largest number is 990
+**/
+/*
+Syntax used here
+if (condition1) {
+    // code to execute if condition1 is true
+} else if (condition2) {
+    // code to execute if condition2 is true
+} else if (condition3) {
+    // code to execute if condition3 is true
+} else {
+    // code to execute if none of the above conditions are true
+}
 */
+#include<stdio.h>
+int main(){
+    int num1,num2,num3;
+    printf("Enter three numbers: ");
+    scanf("%d" "%d" "%d", &num1,&num2,&num3);
+    if (num1>num2 && num1>num3){
+        printf("The largest number is %d",num1);
+    } else if(num2>num3){
+        printf("The largest number is %d",num2);
+    }
+    else{
+            printf("The largest number is %d",num3);
+    }
+getch();
+return 0;
+}
