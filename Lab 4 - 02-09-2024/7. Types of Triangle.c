7337a9875a0a46330ff26743a9b27343199af12e
MrRogueKnight <89287357+MrRogueKnight@users.noreply.github.com>
2024-09-02 14:56:08 +0530

    Rogue

---
+++ Lab 4 - 02-09-2024/7. Types of Triangle.c
@@ -0,0 +1,8 @@
+/**
+Title: Types of Triangle
+WAP to check whether the triangle is equilateral, isosceles or scalene. (Triangle consists of three sides of provided lengths n1, n2 and n3 units).
+
+Input:   (Set1: n1=3, n2=3, n3=4) 	(Set2: n1=4, n2=4, n3=4) 	(Set3: n1=4, n2 =5, n3=7)
+Output:  (Set1: Isosceles) 		(Set2: Equilateral)		(Set3: Scalene)
+
+**/
