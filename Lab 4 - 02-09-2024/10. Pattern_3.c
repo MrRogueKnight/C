6016afad817a12bb3d4e3094fdf92016389fc3e5
MrRogueKnight <89287357+MrRogueKnight@users.noreply.github.com>
2024-09-02 15:36:16 +0530

    Rogue

--- Lab 4 - 02-09-2024/10. Pattern_3.c
+++ Lab 4 - 02-09-2024/10. Pattern_3.c
@@ -1,13 +1,12 @@
 /**
-Title: Pattern 1
+Title: Pattern 2
 WAP to print the following pattern
 Input: Enter the value of n:5
 --------------
-Output:
-*********
-*******
-*****
-***
 *
+***
+*****
+*******
+*********
 --------------
 */
