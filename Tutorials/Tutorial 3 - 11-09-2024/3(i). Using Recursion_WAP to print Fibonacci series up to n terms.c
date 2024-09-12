b227d091b0b5851ccb67b63f5fd18301d2b60fd5
MrRogueKnight <89287357+MrRogueKnight@users.noreply.github.com>
2024-09-12 18:15:25 +0530

    Rogue

--- Tutorials/Tutorial 3 - 11-09-2024/3(i). Using Recursion_WAP to print Fibonacci series up to n terms.c
+++ Tutorials/Tutorial 3 - 11-09-2024/3(i). Using Recursion_WAP to print Fibonacci series up to n terms.c
@@ -1,30 +1,26 @@
 #include <stdio.h>
 
-// Function to print Fibonacci series using recursion
-void fibonacci_recursive(int n, int t1, int t2) {
+void printFibonacciRecursive(int n) {
     if (n <= 0) {
         printf("Input should be a positive integer\n");
         return;
     }
-    if (n == 1) {
-        printf("%d ", t1);
-        return;
-    }
+    int t1 = 0, t2 = 1;
     printf("%d %d ", t1, t2);
-    fibonacci_recursive(n - 1, t2, t1 + t2);
+    printFibonacciRecursiveUtil(n - 2, t1, t2);
 }
 
-void print_fibonacci_recursive(int n) {
-    printf("The Fibonacci series up to %d terms is: ", n);
-    fibonacci_recursive(n, 0, 1);
-    printf("\n");
+void printFibonacciRecursiveUtil(int n, int t1, int t2) {
+    if (n <= 0) return;
+    int nextTerm = t1 + t2;
+    printf("%d ", nextTerm);
+    printFibonacciRecursiveUtil(n - 1, t2, nextTerm);
 }
 
 int main() {
     int n;
     printf("Enter the number of terms: ");
     scanf("%d", &n);
-    print_fibonacci_recursive(n);
-
+    printFibonacciRecursive(n);
     return 0;
 }
