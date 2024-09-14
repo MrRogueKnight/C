4a052ac367a8599ba62a479855a2a50b557c52d5
MrRogueKnight <89287357+MrRogueKnight@users.noreply.github.com>
2024-09-14 23:09:13 +0530

    Rogue

---
+++ Problems/Chapter 2/Problem 2/Problem 2.2.c
@@ -0,0 +1,15 @@
+/*
+Problem 2.2
+If a five-digit number is input through the keyboard, write a program to reverse the number.
+Output:
+Enter a five digit number (less than 32767): 12345
+The reversed number is 54321
+*/
+#indlude<stdio.h>
+int main(){
+    int num, reversed_number;
+    printf("Enter a five digit number (less than 32767): ");
+
+    printf("\n The reversed number is %d");
+    return 0;
+}
